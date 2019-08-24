63db0583cfc6a0f1716c22f5ad16bcdc34d8c042
Helton Isac <helton.isac@gmail.com>
2019-08-24 11:20:56 -0300

    Resolvendo Lints, migrando para AndroidX e Atualizando para Android 10

--- app/src/main/java/br/com/alura/agenda/model/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -1,8 +1,8 @@
 package br.com.alura.agenda.model;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.annotation.NonNull;
 
 import java.io.Serializable;
 import java.util.Calendar;
