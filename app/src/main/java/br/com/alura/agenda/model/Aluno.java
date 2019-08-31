80fc7008e39e801735a3a5b98d283636a622c1ca
Helton Isac <helton.isac@gmail.com>
2019-08-31 18:51:04 -0300

    Suportando mais de um Telefone

--- app/src/main/java/br/com/alura/agenda/model/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -13,7 +13,8 @@ public class Aluno implements Serializable {
     @PrimaryKey(autoGenerate = true)
     private int id = 0;
     private String nome;
-    private String telefone;
+    private String telefoneFixo;
+    private String telefoneCelular;
     private String email;
     private Calendar momentoDeCadastro = Calendar.getInstance();
 
@@ -25,46 +26,53 @@ public class Aluno implements Serializable {
         this.momentoDeCadastro = momentoDeCadastro;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getNome() {
         return nome;
     }
 
-    public String getTelefone() {
-        return telefone;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @NonNull
     @Override
     public String toString() {
-        return nome + " - " + telefone;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+        return nome + " - " + telefoneFixo;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public boolean temIdValido() {
         return id > 0;
     }
 
+    public String getTelefoneFixo() {
+        return telefoneFixo;
+    }
+
+    public void setTelefoneFixo(String telefoneFixo) {
+        this.telefoneFixo = telefoneFixo;
+    }
+
+    public String getTelefoneCelular() {
+        return telefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
+    }
 }
